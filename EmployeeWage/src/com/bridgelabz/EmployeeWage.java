be16e32f65558e53d414f607e0f8a2812aba0e9f
Nishit <nishitranjan41@gmail.com>
2022-07-08 23:01:34 +0530

    This commit is for daily employee wages

--- EmployeeWage/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/EmployeeWage.java
@@ -1,16 +1,29 @@
 package com.bridgelabz;
-public class EmployeeWage {
-        static final int Employee_Present=1;
+
+    public class EmployeeWage {
+        static final int IS_FULL_TIME=1;
+        static final int IS_PART_TIME=2;
+        static final int WAGE_PER_HR=20;
+        static final int FULL_DAY_HR=16;
+        static final int PART_TIME_HR=8;
         public static void main(String[] args) {
             System.out.println("Welcome to Employee Wage !");
-            double empCheck=Math.floor(Math.random()*10) % 2;
-            if(empCheck==Employee_Present)
-                System.out.println("employee is present");
-            else
+            double empCheck=Math.floor(Math.random()*10) % 3;
+            int dailywage=0;
+            if(empCheck==IS_FULL_TIME) {
+                System.out.println("employee is full time");
+                dailywage=WAGE_PER_HR * FULL_DAY_HR;
+            } else if (empCheck==IS_PART_TIME) {
+                System.out.println("employee is part time");
+                dailywage=WAGE_PER_HR * PART_TIME_HR;
+            }
+            else {
                 System.out.println("employee is absent");
-
+            }
+            System.out.println("Daily Wage is "+dailywage);
         }
     }
+
 
 
 
